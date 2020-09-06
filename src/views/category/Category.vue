15c9c346791316a631b2d995035efe81b12fa230
ymj132568 <69382176+ymj132568@users.noreply.github.com>
2020-09-06 19:25:39 +0800

    Update Category.vue

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -50,7 +50,7 @@
       }
     },
     created() {
-		  // 1.请求分类数据
+		 
       this._getCategory()
     },
     computed: {
@@ -66,9 +66,9 @@
     methods: {
 		  _getCategory() {
 		    getCategory().then(res => {
-		      // 1.获取分类数据
+		      
 		      this.categories = res.data.category.list
-          // 2.初始化每个类别的子数据
+          
           for (let i = 0; i < this.categories.length; i++) {
             this.categoryData[i] = {
               subcategories: {},
@@ -79,7 +79,7 @@
               }
             }
           }
-          // 3.请求第一个分类的数据
+         
           this._getSubcategories(0)
         })
       },
@@ -95,18 +95,16 @@
         })
       },
       _getCategoryDetail(type) {
-		    // 1.获取请求的miniWallkey
+		    
         const miniWallkey = this.categories[this.currentIndex].miniWallkey;
-        // 2.发送请求,传入miniWallkey和type
+      
 		    getCategoryDetail(miniWallkey, type).then(res => {
-		      // 3.将获取的数据保存下来
+		      
 		      this.categoryData[this.currentIndex].categoryDetail[type] = res
           this.categoryData = {...this.categoryData}
         })
       },
-      /**
-       * 事件响应相关的方法
-       */
+      
       selectItem(index) {
         this._getSubcategories(index)
       }
