8cb7ba53f6448d32bf5119d75d0c5b6f637e5ebc
ymj132568 <69382176+ymj132568@users.noreply.github.com>
2020-09-06 19:02:14 +0800

    Update GridView.vue

--- src/components/common/gridView/GridView.vue
+++ src/components/common/gridView/GridView.vue
@@ -37,16 +37,11 @@
     },
     methods: {
 		  _autoLayout: function () {
-        // 1.获取gridEl和children
-        // 注: 这里为什么不用document.querySelector呢?
-        // 答: 因为如果在项目中, 多处都用到了grid-view, 那么这里就不确定获取的是哪一个了.
         let gridEl = this.$refs.gridView;
         let children = gridEl.children;
 
-        // 2.设置gridEl的内边距
         gridEl.style.padding = `${this.vMargin}px ${this.hMargin}px`
 
-        // 3.计算item的宽度
         let itemWidth = (gridEl.clientWidth - 2 * this.hMargin - (this.cols - 1) * this.itemSpace) / this.cols;
         for (let i = 0; i < children.length; i++) {
           let item = children[i];
